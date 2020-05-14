5dbe93879605dd94a2b6e91942e5c6ba45e5f24d
Gabriel Cousin <7649529+GabrielCousin@users.noreply.github.com>
2020-05-14 14:15:31 +0200

    Add Sentry

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as Sentry from '@sentry/browser';
+import { Vue as VueIntegration } from '@sentry/integrations';
+
+Sentry.init({
+  dsn: process.env.VUE_APP_SENTRY_DSN,
+  integrations: [
+    new VueIntegration({ Vue, attachProps: true })
+  ],
+});
 
 Vue.config.productionTip = false
 
