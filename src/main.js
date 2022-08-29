57e5e87921daffe2b2f29761daf543ff314baf73
Gabriel Cousin <7649529+GabrielCousin@users.noreply.github.com>
2022-08-29 13:19:44 +0200

    Fix Sentry

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import * as Sentry from '@sentry/vue'
 
-const app = createApp(App).mount('#app')
+const app = createApp(App)
 
 Sentry.init({
   app,
   dsn: import.meta.env.VUE_APP_SENTRY_DSN,
 })
+
+app.mount('#app')
